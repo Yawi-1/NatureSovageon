08f4eedea601ac26c38b36305e812ebdbf757dc9
Yawi <yawimalik786@gmail.com>
2025-04-09 13:51:43 +0530

    added ejs

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ const productRouter = require('./routes/product.route')
 const authROuter = require('./routes/auth.route')
 
 
-
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api',productRouter)
 app.use('/api/auth',authROuter)
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.listen(port, () => {
